8f9d3d4e25bd3d5955a6c017d535513718843b70
devGumidafe <devgumidafe@gmail.com>
2025-04-11 13:43:32 +0100

    refactor

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,9 @@
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/js/bootstrap.bundle.js';
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.bundle.js'
 import './assets/main.css'
 
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
 import App from './App.vue'
 
 const app = createApp(App)
-
-app.use(createPinia())
-
 app.mount('#app')
